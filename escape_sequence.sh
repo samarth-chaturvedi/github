680fb55c8a7a2fb01b0f134016efd321382d0ac7
samarth <samarth07@hotmail.com>
2017-04-24 14:42:50 +0530

    escapping sequence
    Signed-off-by: samarth <samarth07@hotmail.com>

---
+++ escape_sequence.sh
@@ -0,0 +1,4 @@
+echo 'Hello world, how are you'
+echo 'half sentence in new line- Hello world, \nhow are you'
+echo 'One tab apart- Hello world, \thow are you'
+echo 'escaping previous word Hello world, \b\b\b\b\b\b\bhow are you'
